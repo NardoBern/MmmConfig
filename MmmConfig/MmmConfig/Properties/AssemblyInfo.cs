61b5cbc7dbabec2df0b3771135d710819a1e43b2
Nardo Bern <g.parodi@arol.it>
2022-04-21 15:43:16 +0200

    Modify Assembly.cs for version 1.0.0.100 - Setup compiled for beta version 1.0.0.100

--- MmmConfig/MmmConfig/Properties/AssemblyInfo.cs
+++ MmmConfig/MmmConfig/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
 // usando l'asterisco '*' come illustrato di seguito:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.100")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
